406ca2daeb394245e84c55a705c7f168e86839c6
wannakon2005-ship-it <wannakon2005@gmail.com>
2026-04-20 20:07:58 +0700

    Add files via upload

---
+++ ProjectW/myproject/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>ICT12367 SPU</h1>")
+
+def about(request):
+    return HttpResponse("<h1>เกี่ยวกับเรา</h1>")
+
+def form(request):
+    return render(request, 'form.html')
+
+def contact(request):
+    return HttpResponse("<h1>ติดต่อ วรรณกร มากทิม 68096628</h1>")
